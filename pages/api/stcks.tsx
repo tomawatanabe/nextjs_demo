3a6ae179b7e59531ed8e64c1cba6b3ed690d5e38
Miho_Hayakawa <miho.hayakawa@rakus-partners.co.jp>
2022-11-25 11:23:20 +0900

    DBにstckを追加

--- pages/api/stcks.tsx
+++ pages/api/stcks.tsx
@@ -1,7 +1,16 @@
 
-export default async function addObj(){
+export default async function AddObj(){
 
-        const items = await fetch('http://localhost:8000/items');
-        const stock = await fetch("http://localhost:8000/stock");
-        
-    };
+
+    const item = await fetch('http://localhost:8000/items');
+        const itemsdata:[items:{id:number}] = await item.json()    
+    const stock = await fetch("http://localhost:8000/stock");
+        const stocksdata:[stock:{id:number}]= await stock.json()
+
+    //.filterでIDが一致するもののみの配列を作成
+    const addObj = itemsdata.filter((stocksdata,itemsdata) => 
+        stocksdata.id === itemsdata);
+
+    console.log(addObj);
+
+ };
