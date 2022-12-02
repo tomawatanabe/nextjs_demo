fc10bdff6fd73cf86e4018a59c265a7b9bdb1e17
toma <toma.watanabe@rakus-partners.co.jp>
2022-12-02 20:12:32 +0900

    商品詳細ページのCSSを修正

--- pages/[id].tsx
+++ pages/[id].tsx
@@ -31,69 +31,75 @@ export default function Detail({ stock }: { stock: Stock }) {
     <div>
       <Header />
       <div>
-        <div className="top-wrapper">
-          <h1>{stock.item.name}</h1>
-          <p>年代：{stock.item.year}年代</p>
-          <p>色：{stock.item.color}</p>
-        </div>
-        <div className="main-content">
-          <div className="image-wrapper">
-            <div className="main-image">
-              <Image
-                src={`/${stock.image1}`}
-                height={200}
-                width={200}
-                alt={stock.item.name}
-                priority
-              />
-            </div>
-            <div className="sub-images">
-              <Image
-                className="sub-image"
-                src={`/${stock.image2}`}
-                height={200}
-                width={200}
-                alt={stock.item.name}
-                priority
-              />
-              <Image
-                className="sub-image"
-                src={`/${stock.image3}`}
-                height={200}
-                width={200}
-                alt={stock.item.name}
-                priority
-              />
-              <Image
-                className="sub-image"
-                src={`/${stock.image4}`}
-                height={200}
-                width={200}
-                alt={stock.item.name}
-                priority
-              />
-              <Image
-                className="sub-image"
-                src={`/${stock.image5}`}
-                height={200}
-                width={200}
-                alt={stock.item.name}
-                priority
-              />
+        <body className="content-wrapper">
+          <div className="top-wrapper">
+            <h1>{stock.item.name}</h1>
+            <div className="sub">
+              <h3>YEAR：{stock.item.year}年代</h3>
+              <h3>COLOR：{stock.item.color}</h3>
             </div>
           </div>
-          <div className="right-side-wrapper">
-            <p>価格：¥{stock.price}</p>
-            <p>サイズ：{stock.size}</p>
-            <p>在庫数：{stock.amount}</p>
-            <p>コンディション：{stock.condition}</p>
-            <ToggleFavButton stock={stock} />
+          <div className="main-content">
+            <div className="image-wrapper">
+              <div className="main-image">
+                <Image
+                  src={`/${stock.image1}`}
+                  height={200}
+                  width={200}
+                  alt={stock.item.name}
+                  priority
+                />
+              </div>
+              <div className="sub-images">
+                <Image
+                  className="sub-image"
+                  src={`/${stock.image2}`}
+                  height={200}
+                  width={200}
+                  alt={stock.item.name}
+                  priority
+                />
+                <Image
+                  className="sub-image"
+                  src={`/${stock.image3}`}
+                  height={200}
+                  width={200}
+                  alt={stock.item.name}
+                  priority
+                />
+                <Image
+                  className="sub-image"
+                  src={`/${stock.image4}`}
+                  height={200}
+                  width={200}
+                  alt={stock.item.name}
+                  priority
+                />
+                <Image
+                  className="sub-image"
+                  src={`/${stock.image5}`}
+                  height={200}
+                  width={200}
+                  alt={stock.item.name}
+                  priority
+                />
+              </div>
+            </div>
+            <div className="right-side-wrapper">
+              <p>価格：¥{stock.price}</p>
+              <p>サイズ：{stock.size}cm</p>
+              <p>在庫数：{stock.amount}</p>
+              <p>コンディション：{stock.condition}</p>
+              <ToggleFavButton stock={stock} />
+            </div>
           </div>
-        </div>
-        <div className="explanation-wrapper">
-          <h2>商品説明</h2>
-          <p className="explanation">{stock.item.description}</p>
-        </div>
+          <div className="explanation-wrapper">
+            <h2>商品説明</h2>
+            <div className="sub description">
+              <p>{stock.item.description}</p>
+            </div>
+          </div>
+        </body>
         <Footer />
       </div>
     </div>
