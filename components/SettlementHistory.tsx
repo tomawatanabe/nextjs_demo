538d3b542cd9df06585a0932f96d3b065ef2726b
toma <toma.watanabe@rakus-partners.co.jp>
2022-12-03 14:25:22 +0900

    マイページの購入履歴とお気に入り一覧に - 降順で表示 - 最近と全体で表示を切り替えるボタン を実装

--- components/SettlementHistory.tsx
+++ components/SettlementHistory.tsx
@@ -1,5 +1,5 @@
 import useSWR from "swr";
-import React from "react";
+import React, { useState } from "react";
 import { Order, Stock } from "../types";
 import { useCookie } from "./useCookie";
 
@@ -8,6 +8,7 @@ const fetcher = (resource: RequestInfo | URL, init: RequestInit | undefined) =>
 
 function SettlementHistory() {
   const cookie = useCookie();
+  const [flag, setFlag] = useState(true);
 
   const { data, error } = useSWR(
     `http://localhost:8000/order?userID=${cookie}`,
@@ -16,12 +17,59 @@ function SettlementHistory() {
   if (error) return <div>failed to load</div>;
   if (!data) return <div>loading...</div>;
 
-  if (!data) {
-    return <p>購入履歴はありません</p>;
+  //idの降順で並び替える
+  const sortedData = data.sort((a: Order, b: Order) => {
+    return a.id < b.id ? 1 : -1;
+  });
+
+  //降順のTop3を抽出した配列を定義
+  const sortTopData = () => {
+    const sortedTopData = [];
+    for (let i = 0; i < 3; i++) {
+      sortedTopData.push({
+        id: sortedData[i]?.id,
+        userId: sortedData[i]?.userId,
+        totalPrice: sortedData[i]?.totalPrice,
+        orderDate: sortedData[i]?.orderDate,
+        note: sortedData[i]?.note,
+        paymentMethod: sortedData[i]?.paymaentMethod,
+        status: sortedData[i]?.status,
+        orderItemList: sortedData[i]?.orderItemList,
+      });
+    }
+    return sortedTopData;
+  };
+
+  const sortedTopData = sortTopData();
+
+  if (!data.length) {
+    return (
+      <>
+        <h2>購入履歴</h2>
+        <p>購入履歴はありません</p>
+      </>
+    );
   } else {
     return (
       <div>
-        <h1>購入履歴</h1>
+        <h2>購入履歴</h2>
+        {flag ? (
+          <>
+            <input
+              type="button"
+              value="全て表示する"
+              onClick={() => setFlag(!flag)}
+            />
+          </>
+        ) : (
+          <>
+            <input
+              type="button"
+              value="最近のみ表示する"
+              onClick={() => setFlag(!flag)}
+            />
+          </>
+        )}
         <table>
           <thead>
             <tr>
@@ -32,24 +80,50 @@ function SettlementHistory() {
             </tr>
           </thead>
           <tbody>
-            {data.map((order: Order) => {
-              const item = order.orderItemList.map((stock: Stock) => {
-                return (
-                  <>
-                    <td>{stock.item.name}</td>
-                    <br />
-                  </>
-                );
-              });
-              return (
-                <tr key={order.id}>
-                  <td>{order.orderDate.toString()}</td>
-                  <td>{order.status}</td>
-                  <td>¥{order.totalPrice}</td>
-                  <td>{item}</td>
-                </tr>
-              );
-            })}
+            {flag ? (
+              <>
+                {sortedTopData.map((order: Order) => {
+                  const item = order.orderItemList.map((stock: Stock) => {
+                    return (
+                      <>
+                        {stock.item.name} <br />
+                      </>
+                    );
+                  });
+
+                  return (
+                    <tr key={order.id}>
+                      <td>{order.orderDate.toString()}</td>
+                      <td>{order.status}</td>
+                      <td>¥{order.totalPrice}</td>
+                      <td>{item}</td>
+                    </tr>
+                  );
+                })}
+              </>
+            ) : (
+              <>
+                {data.map((order: Order) => {
+                  const item = order.orderItemList.map((stock: Stock) => {
+                    return (
+                      <>
+                        {stock.item.name}
+                        <br />
+                      </>
+                    );
+                  });
+
+                  return (
+                    <tr key={order.id}>
+                      <td>{order.orderDate.toString()}</td>
+                      <td>{order.status}</td>
+                      <td>¥{order.totalPrice}</td>
+                      <td>{item}</td>
+                    </tr>
+                  );
+                })}
+              </>
+            )}
           </tbody>
         </table>
       </div>
