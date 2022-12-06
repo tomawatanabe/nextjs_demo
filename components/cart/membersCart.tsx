f399516b51297e7ac576133ec2cb54d7dd88e185
Kyosuke-Sugi <116235034+Kyosuke-Sugi@users.noreply.github.com>
2022-12-06 15:49:32 +0900

    Update membersCart.tsx
    membersCartを最新に更新

--- components/cart/membersCart.tsx
+++ components/cart/membersCart.tsx
@@ -1,3 +1,4 @@
+
 import useSWR, { mutate } from "swr";
 import { useEffect, useState } from "react";
 import { useCookie } from "../useCookie";
@@ -14,86 +15,76 @@ const fetcher = (resource: string): Promise<any> =>
 
 
 const Members = () => {
-    const userID = useCookie();
+  const userID = useCookie();
 
-    // ローカルストレージ内のデータがあるか確認
-    const [localData, setLocalData] = useState<any[]>([]);
+  // ローカルストレージ内のデータがあるか確認
+  const [localData, setLocalData] = useState<any[]>([]);
 
-    useEffect(() => {
-        setLocalData(JSON.parse(localStorage.getItem("shoppingCart") || "{}"));
-    }, []);
+  useEffect(() => {
+    setLocalData(JSON.parse(localStorage.getItem("shoppingCart") || "{}"));
+  }, []);
 
-    // サーバ上のデータを取得
-    let { data, error } = useSWR(
-        `http://localhost:8000/shoppingCart?id=${userID}`,
-        fetcher
-    );
+  // サーバ上のデータを取得
+  let { data, error } = useSWR(
+    `http://localhost:8000/shoppingCart?id=${userID}`,
+    fetcher
+  );
 
-    useEffect(() => {
+  useEffect(() => {
+    mutate(`http://localhost:8000/shoppingCart?id=${userID}`);
+  }, []);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+
+  // カート内商品削除
+  const handleDelete = (cart: any, id: any) => {
+    const stock = cart.stock;
+    const deleted = stock.filter((item: Stock) => item.id !== id);
+    console.log(deleted);
+
+    fetch(`http://localhost:8000/shoppingCart/${userID}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        stock: deleted,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
         mutate(`http://localhost:8000/shoppingCart?id=${userID}`);
-    }, []);
+        Router.push("/cart");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
 
-    if (error) return <div>failed to load</div>;
-    if (!data) return <div>loading...</div>;
+  // ログイン前のカート内商品をログイン後のカートに移動
+  const handleCombine = (cart: any) => {
+    const stock = cart.stock;
+    for (const localItem of localData[0]?.stock) {
+      if (stock.some((serverItem: any) => serverItem.id === localItem.id)) {
+        continue;
+      }
+      stock.push(localItem);
+    }
 
-    // カート内商品削除
-    const handleDelete = (cart: any, id: any) => {
-        const stock = cart.stock;
-        const deleted = stock.filter((item: Stock) => item.id !== id);
-        console.log(deleted);
-
-        fetch(`http://localhost:8000/shoppingCart/${userID}`, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                stock: deleted,
-            }),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log("Success:", data);
-                mutate(`http://localhost:8000/shoppingCart?id=${userID}`);
-                Router.push("/cart");
-            })
-            .catch((error) => {
-                console.error("Error:", error);
-            });
-    };
-
-    // ログイン前のカート内商品をログイン後のカートに移動
-    const handleCombine = (cart: any) => {
-        const stock = cart.stock;
-        for (const localItem of localData[0]?.stock) {
-            if (stock.some((serverItem: any) => serverItem.id === localItem.id)) {
-                continue;
-            }
-            stock.push(localItem);
-        }
-
-        fetch(`http://localhost:8000/shoppingCart/${userID}`, {
-            method: "PATCH",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                stock: stock,
-            }),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log("Success:", data);
-                localStorage.clear();
-                Router.reload();
-            })
-            .catch((error) => {
-                console.error("Error:", error);
-            });
-    };
-
-    // ログイン前のカート内商品をログイン後のカートに移動したくない場合
-    const rejectCombine = () => {
+    fetch(`http://localhost:8000/shoppingCart/${userID}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        stock: stock,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
         localStorage.clear();
         Router.reload();
       })
@@ -155,3 +146,4 @@ const Members = () => {
 };
 
 export default Members;
+
