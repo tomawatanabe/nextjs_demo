6b878cbf507c4dca775eaa81eaa43a6404bee3dd
kyokahayashi <20qwmnpozxfg18@gmail.com>
2022-12-06 10:09:02 +0900

    プルしたもの

--- components/cart/membersCart.tsx
+++ components/cart/membersCart.tsx
@@ -1,91 +1,147 @@
-
-import { useState, useEffect } from "react";
+import useSWR, { mutate } from "swr";
+import { useEffect, useState } from "react";
+import { useCookie } from "../useCookie";
+import CartItem from "./cartItem";
+import CartTotal from "./cartTotal";
 import Router from "next/router";
 import type { Stock } from "../../types";
-import { useCookie } from "../useCookie";
-import useSWR from "swr";
-import styles from "../../styles/CartButton.module.css";
+import cart from "../../pages/api/cart";
+import Link from "next/link";
+import styles from "../../styles/Cart.module.css";
 
 const fetcher = (resource: string): Promise<any> =>
     fetch(resource).then((res) => res.json());
 
-const CartButton = ({ stock }: { stock: Stock }) => {
+
+const Members = () => {
     const userID = useCookie();
 
-    const { data, mutate } = useSWR(
-        `http://localhost:8000/shoppingCart/${userID}`,
-        fetcher
-    );
+    // ローカルストレージ内のデータがあるか確認
     const [localData, setLocalData] = useState<any[]>([]);
-
-    console.log(stock);
-
-    const dataType = {
-        stock: [stock],
-    };
 
     useEffect(() => {
         setLocalData(JSON.parse(localStorage.getItem("shoppingCart") || "{}"));
     }, []);
 
-    const addCartItem = async () => {
-        if (!userID === true) {
-            // ログアウト状態でカートに商品追加
-            if (localStorage.getItem("shoppingCart")) {
-                const target = stock;
+    // サーバ上のデータを取得
+    let { data, error } = useSWR(
+        `http://localhost:8000/shoppingCart?id=${userID}`,
+        fetcher
+    );
 
-                if (localData[0].stock.some((item: any) => item.id === target.id)) {
-                    alert("既にカートに追加済みです");
-                    return;
-                } else {
-                    localData[0].stock.push(stock);
-                    localStorage.setItem("shoppingCart", JSON.stringify(localData));
-                    setLocalData(
-                        JSON.parse(localStorage.getItem("shoppingCart") || "{}")
-                    );
-                }
-            } else {
-                localStorage.setItem("shoppingCart", JSON.stringify([dataType]));
-                setLocalData(JSON.parse(localStorage.getItem("shoppingCart") || "{}"));
-            }
-        } else {
-            // ログイン状態でカート商品追加
-            const res = await fetch(`http://localhost:8000/shoppingCart/${userID}`);
-            const user = await res.json();
-            const target = stock;
+    useEffect(() => {
+        mutate(`http://localhost:8000/shoppingCart?id=${userID}`);
+    }, []);
 
-            if (user?.stock.some((item: any) => item.id === target.id)) {
-                alert("既にカートに追加済みです");
-                return;
-            } else {
-                fetch("/api/cart", {
-                    method: "POST",
-                    headers: { "content-type": "application/json" },
-                    body: JSON.stringify({ stockID: stock.id }),
-                });
-                mutate(`http://localhost:8000/shoppingCart/${userID}`);
-                Router.reload();
+    if (error) return <div>failed to load</div>;
+    if (!data) return <div>loading...</div>;
+
+    // カート内商品削除
+    const handleDelete = (cart: any, id: any) => {
+        const stock = cart.stock;
+        const deleted = stock.filter((item: Stock) => item.id !== id);
+        console.log(deleted);
+
+        fetch(`http://localhost:8000/shoppingCart/${userID}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                stock: deleted,
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Success:", data);
+                mutate(`http://localhost:8000/shoppingCart?id=${userID}`);
+                Router.push("/cart");
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    };
+
+    // ログイン前のカート内商品をログイン後のカートに移動
+    const handleCombine = (cart: any) => {
+        const stock = cart.stock;
+        for (const localItem of localData[0]?.stock) {
+            if (stock.some((serverItem: any) => serverItem.id === localItem.id)) {
+                continue;
             }
+            stock.push(localItem);
         }
+
+        fetch(`http://localhost:8000/shoppingCart/${userID}`, {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                stock: stock,
+            }),
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                console.log("Success:", data);
+                localStorage.clear();
+                Router.reload();
+            })
+            .catch((error) => {
+                console.error("Error:", error);
+            });
+    };
+
+    // ログイン前のカート内商品をログイン後のカートに移動したくない場合
+    const rejectCombine = () => {
+        localStorage.clear();
+        Router.reload();
     };
 
     return (
-        <div>
-            {data?.stock?.some((item: any) => item.id === stock.id) ||
-                localData[0]?.stock.some((item: any) => item.id === stock.id) ? (
-                <button className={styles.addedCartBtn} onClick={addCartItem} disabled>
-                    カートに追加済み
-                </button>
-            ) : (
-                <button
-                    className={styles.addCartBtn}
-                    onClick={addCartItem}
-                >
-                    カートへ追加
-                </button>
-            )}
-        </div>
+        <>
+            <div
+                className={styles.attention}
+                style={{
+                    display: localData[0]?.stock.length ? "block" : "none",
+                }}
+            >
+                <div className={styles.frame}>
+                    <div className={styles.frame_title}>注意！</div>
+                    <p>
+                        ログイン前のカートに商品があります。
+                        <br /> 現在のアカウントのカートにその商品を移動しますか？
+                    </p>
+                    <ul>
+                        {localData[0]?.stock.map((cartItem: Stock) => {
+                            <li>{cartItem?.item.name}</li>;
+                        })}
+                    </ul>
+                    <button
+                        className={styles.yes_btn}
+                        onClick={() => handleCombine(data[0])}
+                    >
+                        はい
+                    </button>
+                    <button className={styles.no_btn} onClick={() => rejectCombine()}>
+                        いいえ
+                    </button>
+                </div>
+            </div>
+            <CartItem
+                data={data}
+                handleDelete={handleDelete}
+                handleCombine={handleCombine}
+                localData={localData}
+            />
+            <CartTotal data={data} />
+            <div style={{ display: data[0]?.stock.length ? "block" : "none" }}>
+                <Link href="#" legacyBehavior>
+                    購入手続きへ進む
+                </Link>
+            </div>
+        </>
     );
 };
 
-export default CartButton;
+export default Members;
