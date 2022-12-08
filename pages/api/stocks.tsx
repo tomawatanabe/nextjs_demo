a696420bfd5506013bb05e0af9fef838104751c2
kyokahayashi <20qwmnpozxfg18@gmail.com>
2022-12-08 18:16:15 +0900

    12/9 9:27

--- pages/api/stocks.tsx
+++ pages/api/stocks.tsx
@@ -2,9 +2,9 @@ import { Item } from "../../types";
 import { Stock } from "../../types";
 
 export default async function Stocks() {
-  const item = await fetch("${process.env. NEXT_PUBLIC_API}/api/items");
+  const item = await fetch("http://localhost:8000/items");
   const itemsdata = await item.json();
-  const stock = await fetch("${process.env. NEXT_PUBLIC_API}/api/stock");
+  const stock = await fetch("http://localhost:8000/stock");
   const stocksdata = await stock.json();
 
   stocksdata.map((stock: Stock) => {
@@ -12,7 +12,7 @@ export default async function Stocks() {
     const items = itemsdata.filter((item: Item) => stock.itemID === item.id);
     stock.item = items[0];
 
-    fetch(`${process.env.NEXT_PUBLIC_API}/api/stock/${stock.itemID}`, {
+    fetch(`http://localhost:8000/stock/${stock.itemID}`, {
       method: "PUT",
       headers: {
         "Content-Type": "application/json",
