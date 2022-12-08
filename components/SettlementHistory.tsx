76515e15788d43177fc059198b1195491c0f4923
HayakawaM <114966638+Hayaka3a@users.noreply.github.com>
2022-12-08 17:28:11 +0900

    Update SettlementHistory.tsx

--- components/SettlementHistory.tsx
+++ components/SettlementHistory.tsx
@@ -135,7 +135,7 @@ function SettlementHistory() {
                     <tr key={order.id}>
                       <td className={styles.td_center}>{order.shipStatus}</td>
                       <td className={styles.td_center}>
-                        {order.orderDate.toString()}
+                        {order.orderDate}
                       </td>
                       <td className={styles.td_center}>
                         ¥{order.totalPrice.toLocaleString()}
