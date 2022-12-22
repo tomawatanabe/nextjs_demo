5388221e00bd91538594f6a671efe9450e6580e5
toma <toma.watanabe@rakus-partners.co.jp>
2022-12-22 19:53:34 +0900

    api

--- pages/api/getUserImfo.ts
+++ pages/api/getUserImfo.ts
@@ -2,16 +2,22 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { supabase } from "../../lib/supabase-client";
 
 const getUserImfo = async (req: NextApiRequest, res: NextApiResponse) => {
+  const cookieNumber = Number(req.cookies.userID);
+  // const cookieInt = parseInt(req.cookies.userID, 10);
+
   const { data, error } = await supabase
     .from("users")
-    .select()
-    .eq("id", Number(req.cookies.userID))
+    .select("*")
+    .eq("id", cookieNumber)
     .limit(1)
     .single();
 
-  // 401 Unauthorized、認証が必要
-  if (error) return res.status(401).json({ error: error.message });
+  console.log(cookieNumber);
 
+  // 401 Unauthorized、認証が必要
+  if (error) res.status(401).json({ error: error.message });
+
+  console.log(error);
   // 200番台は、処理が成功して正常にレスポンスができている状態
   return res.status(200).json(data);
 };
